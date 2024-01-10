c7efc804209029426e59c04f470522b30f227a52
faizalpriotsn <faizalprio.yk1@gmail.com>
2024-01-10 12:04:39 +0700

    tugasuas

---
+++ about.php
@@ -0,0 +1,17 @@
+<section class="py-5">
+    <div class="container">
+        <div class="card rounded-0">
+            <div class="card-body">
+            Berdiri pada Oktober 2023, ONTHELS fokus untuk secara konsisten melayani kebutuhan konsumen akan segala hal yang berkaitan dengan olahraga sepeda dengan mengutamakan kualitas dan keaslian produk dengan didukung oleh sistem manajemen yang baik dan pengetahuan teknis yang kuat.
+
+ONTHELS memiliki misi untuk menjadi  penjual sepeda modern terbaik dengan pilihan produk yang lengkap dan berkualitas dengan harga yang terpercaya,kompetitif,dan pelayanan yang profesional kepada konsumen.
+
+
+
+
+
+
+            </div>
+        </div>
+    </div>
+</section>
